cf65d7eb390cba9640d15ab0acb52fc04a877a0d
Andrew Burnett <andrew@andburn.info>
2019-07-13 23:19:17 +0100

    Sort corner choice to match output

--- board_detection.py
+++ board_detection.py
@@ -32,34 +32,47 @@ def find_nearest(points, p):
     nearest = [0, 0]
     ndist = -1
     for pt in points:
-        dist = get_distance(p, pt[0])
+        dist = get_distance(p, pt)
         if ndist < 0 or dist < ndist:
             ndist = dist
             nearest = pt
     return nearest
 
 
+def sort_corners(points):
+    assert len(points) == 4
+    sort_y = sorted(points, key=lambda p: p[1])
+    sort_y[:2] = sorted(sort_y[:2], key=lambda p: p[0])
+    sort_y[2:] = sorted(sort_y[2:], key=lambda p: p[0])
+    return sort_y
+
+
 # based on manual corners transform perspective
 def adjust_image(img, mask, points, size):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
     # use Harris/Shi-Tomasi corner detector on image
     corners = cv.goodFeaturesToTrack(gray, 85, 0.04, 8, mask=mask)
+    # flatten out each corner to a 2 dim array
+    corners = [i.ravel() for i in corners]
 
-    # transform perspective
     corner_points = []
+    # get the nearest corners to selected points
     for p in points:
         corner_points.append(find_nearest(corners, p))
 
-    # show corners
+    # display the corner location on the image
     for i in corners:
         x, y = i.ravel()
         cv.circle(gray, (x,y), 3, 255, -1)
         for c in corner_points:
-            if x == c[0][0] and y == c[0][1]:
+            if x == c[0] and y == c[1]:
                 cv.circle(gray, (x,y), 3, 0, -1)
     cv.imwrite('captures/_corners.jpg', gray)
 
-    corner_points = np.float32(corner_points)
+    # sort corners so they are TL, TR, BL, BR
+    corner_points = np.float32(sort_corners(corner_points))
+    # transform the perspective
     transform = cv.getPerspectiveTransform(corner_points, output_dims)
     output = cv.warpPerspective(img, transform, (output_size, output_size))
     cv.imwrite('captures/_perspective.jpg', output)
